02a73b245cfe81c9a23b90bd44b0454d1e7719ba
dimitrisx26 <dxynos@outlook.com>
2026-06-21 12:34:47 +0300

    feat: update app component to use apiData() method and integrate HttpClient for data fetching

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    JsonPipe
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  /**
+   * Provides the HttpClient service for making HTTP requests.
+   */
+  private http = inject(HttpClient);
+
+  /**
+   * Data coming from the API
+   */
+  apiData = signal<any>(null);
+
+  /**
+   * Initializes the component.
+   */
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
+    this.http.get('http://127.0.0.1:8000/max/track/Spa/fastest-lap')
+      .subscribe({
+        next: (response) => {
+          this.apiData.set(response);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+  }
 }
